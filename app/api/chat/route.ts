deebf0a60bb6cecc284920792b6d24efc4af321d
Meathill <meathill@gmail.com>
2023-11-27 21:36:40 +0800

    change: use GPT-4 if email matches

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,5 @@
 import { kv } from '@vercel/kv'
+import { get } from '@vercel/edge-config'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import OpenAI from 'openai'
 
@@ -8,26 +9,34 @@ import { nanoid } from '@/lib/utils'
 export const runtime = 'edge'
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: process.env.CF_AI_GATEWAY || 'https://api.openai.com/v1',
 })
 
 export async function POST(req: Request) {
   const json = await req.json()
   const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
+  const user = (await auth())?.user
+  const userId = user?.id;
 
-  if (!userId) {
+  if (!user || !userId) {
     return new Response('Unauthorized', {
       status: 401
     })
   }
 
-  if (previewToken) {
+  const aw = (await get('aw')) as string[]
+  let model = 'gpt-3.5-turbo-1106';
+  console.log('xxx', user.email, userId)
+  if (user.email && aw?.includes(user.email)) {
+    openai.apiKey = process.env.AW_OPENAI_API_KEY || '';
+    model = 'gpt-4-1106-preview'
+  } else if (previewToken) {
     openai.apiKey = previewToken
   }
 
   const res = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model,
     messages,
     temperature: 0.7,
     stream: true
